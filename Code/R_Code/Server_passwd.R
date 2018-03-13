1b3e71ff9115b0190f7b8b1bfc56a94c28c16a0b
Jadamso <jordan.m.adamson@gmail.com>
2018-03-12 20:23:38 -0700

    new R commit

--- Code/R_Code/Server_passwd.R
+++ Code/R_Code/Server_passwd.R
@@ -138,9 +138,12 @@ passwd_maker <- compiler::cmpfun( function(
 #' @rdname passwd_maker
 #' @param n number of student participants
 #' @param passwd what password
+#' @param sys if TRUE execute the bash script
+#' @param passfile what file holds the passwords
 #' @param gdir location of host Game folder
 #' @param pfile name of file to hold the passwords
 #' @param add_admin also reset admin password [NOT YET WORKING]
+#' @param create overwrite and create new passwd_file [CURRENTLY done with new admin]
 
 #' @return a string to be executed by system()
 # @examples
@@ -149,7 +152,11 @@ passwd_maker <- compiler::cmpfun( function(
 
 passwd_maker0 <- compiler::cmpfun( function(
     n=20,
+    user_append="JA",
     passwd="TrialAuction",
+    create=FALSE,
+    sys=FALSE,
+    passfile="/srv/shiny-server/passwd",
     gdir="/etc/shiny-server/",
     pfile="passwd",
     add_admin=TRUE) {
@@ -157,17 +164,20 @@ passwd_maker0 <- compiler::cmpfun( function(
     
     ## Create File with Admin
     passfile <- paste0(gdir,pfile)
-    sspasswd("admin", passwd,
+    SetupGameR::sspasswd("admin", passwd,
         create=TRUE, sys=TRUE, passfile=passfile)
 
     Class <- SetupGameR::create_playergroups(
-        n=n, groupsize=1)[,c("Name", "ID")]
-    Participants <- split(Class, Class$ID)      
+        n=n, groupsize=1)[,"ID"]
+    USERS <- paste0(user_append, USERS)
     
     ## Players created in DoubleAuction/PlayerSetup/PlayerSetup.R
     ## "JA" was appended to each ID
-    sspasswd_cmd(Participants, passwd,
-        create=FALSE, sys=TRUE, passfile=passfile)
+    mapply(SetupGameR::sspasswd, USERS, PASSWDS,
+        MoreArgs=list(
+            create=FALSE,
+            passfile=passfile,
+            sys=sys))
     
     return(passwd)
 
